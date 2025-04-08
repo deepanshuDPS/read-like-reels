2cab81672a4fb7c6b4a8aeacc208b797dc45aa7d
deepanshu-hh <deepanshu.a@hubhopper.com>
2025-04-08 18:09:33 +0530

    title fix

--- app/api/read/[slug]/route.ts
+++ app/api/read/[slug]/route.ts
@@ -1,12 +1,9 @@
-import { createClient } from "@/utils/supabase/server";
-import { NextRequest, NextResponse } from "next/server";
+import {  NextResponse } from "next/server";
 
 
 export async function GET(
-  request: NextRequest,
   { params }: { params: { slug: string } }
 ) {
-  console.log(params); // ✅ { slug: "your-slug" }
   const slug = params.slug || "";
   return NextResponse.json({ slug });
 }
--- app/read/[slug]/page.tsx
+++ app/read/[slug]/page.tsx
@@ -2,16 +2,27 @@ import Pages from "@/components/Pages/Pages";
 import { createClient } from "@/utils/supabase/server";
 
 
+async function fetchWriting(slug:string) {
+  const supabase = createClient();
+  return await supabase.from("writings")
+    .select("*").eq("slug", slug);
+}
+
+export async function generateMetadata({ params }: { params: { slug: string } }) {
+  const { data, error } = await fetchWriting(params.slug);
+  return {
+    title: (data as any[])[0].title,
+    description: (data as any[])[0].text.substring(0, 100) + "...",
+  };
+}
+
 export default async function Index({
   params,
 }: {
   params: { slug: string };
 }){
-  const supabase = createClient();
-  const slug = params.slug;
-  let query = supabase.from("writings")
-    .select("*").eq("slug", slug);
-  const { data, error } = await query;
+  
+  const { data, error } = await fetchWriting(params.slug);
   return (
     <>
       <Pages selectedWriting={(data as any[])[0]} />
