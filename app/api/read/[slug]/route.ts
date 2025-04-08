b56ee3f3c29902a4b387ed3756dbd1c8e527eb68
deepanshu-hh <deepanshu.a@hubhopper.com>
2025-04-08 18:25:06 +0530

    request fix

--- app/api/read/[slug]/route.ts
+++ app/api/read/[slug]/route.ts
@@ -1,7 +1,8 @@
-import {  NextResponse } from "next/server";
+import {  NextRequest, NextResponse } from "next/server";
 
 
 export async function GET(
+  request: NextRequest,
   { params }: { params: { slug: string } }
 ) {
   const slug = params.slug || "";
