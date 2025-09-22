f469895644422a3c577f43aa29d742e259fba0cf
deepanshuDPS <ideepanshu9@gmail.com>
2025-09-22 18:59:40 +0530

    branch fix

--- app/loading.tsx
+++ app/loading.tsx
@@ -4,7 +4,7 @@ export default function Loading() {
     // You can add any UI inside Loading, including a Skeleton.
     return (/* From Uiverse.io by satyamchaudharydev */
         <div className="flex flex-row justify-center items-center w-full h-screen">
-            <div className="spinner my-10">
+            <div className="spinner my-12">
                 <span></span>
                 <span></span>
                 <span></span>
